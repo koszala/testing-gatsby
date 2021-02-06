2836fce404b16583b9bb77e571709b95ef88f369
Paweł Koszała <pawel.koszala@outlook.com>
2021-02-06 23:59:21 +0100

    Fix conditional img render on news template

--- src/templates/newsTemplate.js
+++ src/templates/newsTemplate.js
@@ -5,50 +5,50 @@ import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default function NewsTemplate({ data }) {
-    const { markdownRemark } = data // data.markdownRemark holds your post data
-    const { frontmatter, html, timeToRead } = markdownRemark
-    let featuredImgFluid = frontmatter.featured.childImageSharp.fluid
+  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { frontmatter, html, timeToRead } = markdownRemark
+  let featuredImgFluid = frontmatter.featured ? frontmatter.featured.childImageSharp.fluid : null
 
 
+  return (
+    <Layout>
+      <div className="news-container">
+        <div className="news">
+          <Header headerText={frontmatter.title} />
+          <h2>{frontmatter.date}</h2>
+          <p>Time to read: {timeToRead} min</p>
+          {featuredImgFluid &&
+          <Img fluid={featuredImgFluid} />
+          }
+          <div
+            className="news-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
 
-    return (
-        <Layout>
-            <div className="news-container">
-                <div className="news">
-                    <Header headerText={frontmatter.title}/>
-                    <h2>{frontmatter.date}</h2>
-                    <p>Time to read: {timeToRead} min</p>
-                    <Img fluid={featuredImgFluid} />
-                    
-                    <div
-                        className="news-content"
-                        dangerouslySetInnerHTML={{ __html: html }}
-                    />
-
-                </div>
-            </div>
-            <Link to="/news">Return</Link>
-        </Layout>
-    )
+        </div>
+      </div>
+      <Link to="/news">Return</Link>
+    </Layout>
+  )
 }
 
 export const pageQuery = graphql`
-  query NewsItemQuery($slug: String!) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
-      html
-      frontmatter {
-        date(formatString: "dd, DD MMMM, YYYY")
-        slug
-        title
-        featured {
-          childImageSharp {
-            fluid(maxWidth: 1600) {
-              ...GatsbyImageSharpFluid
+    query NewsItemQuery($slug: String!) {
+        markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+            html
+            frontmatter {
+                date(formatString: "dd, DD MMMM, YYYY")
+                slug
+                title
+                featured {
+                    childImageSharp {
+                        fluid(maxWidth: 1600) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
             }
-          }
+            timeToRead
         }
-      }
-      timeToRead
     }
-  }
 `
